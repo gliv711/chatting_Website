9c53d07dd69ec49599b536cc75630eeb858e0b16
gliv711 <nedermfarrej@fsb.u-carthage.tn>
2021-10-11 17:24:15 +0100

    Correction 2
    First Database Connection

---
+++ registration.php
@@ -0,0 +1,35 @@
+<html>
+<head> <title> test </title> </head>
+<body>
+    <center>
+<?php
+
+//create connection
+
+$conn= mysqli_connect ("localhost","root","","chatting-website");
+
+if($conn==false){
+    die("error, could not connect");
+}
+else echo 'Connected' ;
+
+// form
+$nom=$_REQUEST['nom'];
+$prenom=$_REQUEST['prenom'];
+$email=$_REQUEST['email'];
+$pdw=$_REQUEST['pdw'];
+$tel=$_REQUEST['tel'];
+
+$sql="insert into users(nom,prenom,email,mdp,numtel) values ('$nom','$prenom','$email','$pdw','$tel') " ;
+
+// if succesfull
+if (mysqli_query($conn,$sql)){
+    echo "<h3> Inscription réussie </h3>";
+}
+else echo"<h3> Erreur dans l'inscription </h3>";
+
+mysqli_close($conn);
+?>
+</center>
+</body>
+</html>
