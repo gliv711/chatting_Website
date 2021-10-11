8f99dfa6bad93c5534d0a8a11c7e476c524e0dba
gliv711 <nedermfarrej@fsb.u-carthage.tn>
2021-10-11 16:52:16 +0100

    correction

--- connexion.php
+++ connexion.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Cours PHP / MySQL</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="cours.css">
-    </head>
-    <body>
-        <h1>Bases de données MySQL</h1>  
+
         <?php
             $servername = 'localhost';
             $username = 'root';
@@ -22,5 +14,3 @@
             }
             echo 'Connexion réussie';
         ?>
-    </body>
-</html>
