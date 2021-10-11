4312f661fa7a2f456ab22a4ce1d9ada167063d39
gliv711 <nedermfarrej@fsb.u-carthage.tn>
2021-10-11 20:56:52 +0100

    Update connection.php
    login works fine now

--- php/connection.php
+++ php/connection.php
@@ -7,16 +7,13 @@
 
 
     $success = mysqli_connect ("localhost","root","","chatting-website");
-
-    ?>
-    <?php
     $email = $_REQUEST['email'];
     $password = $_REQUEST['pwd'];
     $sql = mysqli_query($success,"SELECT * FROM users WHERE email = '".$_REQUEST['email']."' and mdp = '".($_REQUEST['pwd'])."'");
     $row = mysqli_num_rows($sql);
     if($row > 0 )
     {
-      session_start();
+    session_start();
       $_SESSION['email'] = $_REQUEST['email'];
       $_SESSION['pwd'] = $_REQUEST['pwd'];
       echo "<h1>login done</h1>";
@@ -24,6 +21,7 @@
       echo "<h2>fail login</h2> ";
     }
 
+    
 
 
 ?>
